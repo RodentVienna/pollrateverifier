a7f98f8c0fb5afc00d6800972363e4d203783d97
Gareth Francis <gfrancis.dev@gmail.com>
2018-08-22 20:43:19 +0100

    Restructure into a class

--- pollrateverifier.cpp
+++ pollrateverifier.cpp
@@ -13,30 +13,82 @@
 
 #include "pollrateverifier.h"
 
+PollrateVerifier::PollrateVerifier()
+{
+  // Open the uinput driver
+  m_fd = open("/dev/uinput", O_WRONLY | O_NONBLOCK);
+  if( m_fd < 0 )
+  {
+    quit("Failed to open /dm_ev/uinput");
+  }
+
+  // Setup our fake input dm_evice
+  memset(&m_uidev, 0, sizeof(m_uidev));
+  snprintf(m_uidev.name, UINPUT_MAX_NAME_SIZE, "uinput-spammer-test");
+  m_uidev.id.bustype = BUS_USB;
+  // These IDs identify the provider and dm_evice attached to the USB bus
+  // In this case just use 0x01. I couldn't find any accepted 'example' value
+  m_uidev.id.vendor  = 0x01;
+  m_uidev.id.product = 0x01;
+  m_uidev.id.version = 1;
+
+  // Register what our input dm_evice can do
+  if( ioctl(m_fd, UI_SET_EVBIT, EV_KEY) < 0 )
+  {
+    quit("ioctl");
+  }
+  if( ioctl(m_fd, UI_SET_KEYBIT, KEY_A) < 0 )
+  {
+    quit("ioctl");
+  }
+
+  // And create the dm_evice
+  if( write(m_fd, &m_uidev, sizeof(m_uidev)) < 0 )
+  {
+    quit("Failed to write to m_uidev");
+  }
+  if( ioctl(m_fd, UI_DEV_CREATE) < 0 )
+  {
+    quit("Failed to create uinput dm_evice");
+  }
+
+}
+
+PollrateVerifier::~PollrateVerifier()
+{
+  // Cleanup and exit
+  if( ioctl(m_fd, UI_DEV_DESTROY) < 0 )
+  {
+    quit( "Failed to destroy input dm_evice" );
+  }
+
+  close(m_fd);
+}
+
 void PollrateVerifier::quit( const char* msg)
 {
   perror(msg);
   exit(1);
 }
 
-void PollrateVerifier::keyPress( bool pressed, int fd, input_event& ev )
+void PollrateVerifier::keyPress( int key, bool pressed )
 {
-  // Register the key press event
-  memset(&ev, 0, sizeof(input_event));
-  ev.type = EV_KEY;
-  ev.code = KEY_A;
-  ev.value = pressed ? 1 : 0;
-  if( write(fd, &ev, sizeof(input_event)) < 0 )
+  // Register the key press m_event
+  memset(&m_ev, 0, sizeof(input_event));
+  m_ev.type = EV_KEY;
+  m_ev.code = KEY_A;
+  m_ev.value = pressed ? 1 : 0;
+  if( write(m_fd, &m_ev, sizeof(input_event)) < 0 )
   {
     quit("Failed to press key");
   }
 
-  // Register the key release event
-  memset(&ev, 0, sizeof(input_event));
-  ev.type = EV_SYN;
-  ev.code = SYN_REPORT;
-  ev.value = 0;
-  if( write(fd, &ev, sizeof(input_event)) < 0 )
+  // Register the key release m_event
+  memset(&m_ev, 0, sizeof(input_event));
+  m_ev.type = EV_SYN;
+  m_ev.code = SYN_REPORT;
+  m_ev.value = 0;
+  if( write(m_fd, &m_ev, sizeof(input_event)) < 0 )
   {
     quit("Failed to release key");
   }
@@ -44,48 +96,8 @@ void PollrateVerifier::keyPress( bool pressed, int fd, input_event& ev )
 
 void PollrateVerifier::run()
 {
-  int fd;
-  uinput_user_dev uidev;
-  input_event ev;
 
-  // Open the uinput driver
-  fd = open("/dev/uinput", O_WRONLY | O_NONBLOCK);
-  if( fd < 0 )
-  {
-    quit("Failed to open /dev/uinput");
-  }
-
-  // Setup our fake input device
-  memset(&uidev, 0, sizeof(uidev));
-  snprintf(uidev.name, UINPUT_MAX_NAME_SIZE, "uinput-spammer-test");
-  uidev.id.bustype = BUS_USB;
-  // These IDs identify the provider and device attached to the USB bus
-  // In this case just use 0x01. I couldn't find any accepted 'example' value
-  uidev.id.vendor  = 0x01;
-  uidev.id.product = 0x01;
-  uidev.id.version = 1;
-
-  // Register what our input device can do
-  if( ioctl(fd, UI_SET_EVBIT, EV_KEY) < 0 )
-  {
-    quit("ioctl");
-  }
-  if( ioctl(fd, UI_SET_KEYBIT, KEY_A) < 0 )
-  {
-    quit("ioctl");
-  }
-
-  // And create the device
-  if( write(fd, &uidev, sizeof(uidev)) < 0 )
-  {
-    quit("Failed to write to uidev");
-  }
-  if( ioctl(fd, UI_DEV_CREATE) < 0 )
-  {
-    quit("Failed to create uinput device");
-  }
-
-  // TODO: Right now let's have a short delay and let the user start evhz in another terminal. Really we want a background thread started to trigger input and a foreground thread to report on the results
+  // TODO: Right now let's have a short delay and let the user start m_evhz in another terminal. Really we want a background thread started to trigger input and a foreground thread to report on the results
   for( auto i = 10u; i > 0; --i )
   {
     std::cout << "Starting input in " << i << " seconds" << std::endl;
@@ -98,18 +110,11 @@ void PollrateVerifier::run()
   {
     // Trigger keydown/up with a small wait in between
     // We're interested in polling rates up to 1Khz so
-    // spam events at twice this rate
-    keyPress( true, fd, ev );
+    // spam m_events at twice this rate
+    keyPress( KEY_A, true );
     usleep(500);
-    keyPress( false, fd, ev );
+    keyPress( KEY_A, false );
     usleep(500);
   }
 
-  // Cleanup and exit
-  if( ioctl(fd, UI_DEV_DESTROY) < 0 )
-  {
-    quit( "Failed to destroy input device" );
-  }
-
-  close(fd);
 }
--- pollrateverifier.h
+++ pollrateverifier.h
@@ -1,12 +1,19 @@
 
-struct input_event;
+#include <linux/input.h>
+#include <linux/uinput.h>
 
 class PollrateVerifier
 {
   public:
+    PollrateVerifier();
+    ~PollrateVerifier();
     void run();
   private:
     void quit( const char* msg );
-    void keyPress( bool pressed, int fd, input_event& ev );
+    void keyPress( int key, bool pressed );
+
+    int m_fd = -1;
+    input_event m_ev;
+    uinput_user_dev m_uidev;
 };
 
